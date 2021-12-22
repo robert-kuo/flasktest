6151c5e781340e4d44b5b683fce3504253619d2b
robert-kuo <kaku@datawise.com.tw>
2021-12-22 22:56:51 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,3 +4,7 @@ myapp = Flask(__name__)
 @myapp.route("/")
 def hello():
     return "Hello Flask, on Azure App Service for Linux!!! ??? $$$ ==============>"
+
+@myapp.route("/test")
+def test():
+    return "function test."
