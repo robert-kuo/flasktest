91b4cdfe09efb1b01487eed2d94bbf1597f77589
robert-kuo <kaku@datawise.com.tw>
2021-12-24 09:39:01 +0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 
 import os
 import Opt_func
+from flask_httpauth import HTTPBasicAuth
 
 myapp = Flask(__name__)
 if os.name == 'nt':
@@ -11,10 +12,22 @@ else:
     mainpath = '/aidata'
     ip = ''
 
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'dw': return 'nthu'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 401)
+
 @myapp.route("/")
 def hello():
     return 'Hello World! ' +  mainpath + 'ip: ' + ip
 
 @myapp.route("/test")
+@auth.login_required
 def test():
     return "function test."
