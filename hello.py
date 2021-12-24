b38f0bba23b5399f77edf8efdfc0c0ee9b71838c
robert-kuo <kaku@datawise.com.tw>
2021-12-24 09:23:57 +0800

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 from flask import Flask
+
+import os
+import Opt_func
+
 myapp = Flask(__name__)
+if os.name == 'nt':
+    mainpath = 'd:\\opt_web'
+    ip = Opt_func.GetIP()
+else:
+    mainpath = '/aidata'
+    ip = ''
 
 @myapp.route("/")
 def hello():
-    return "Hello Flask, on Azure App Service for Linux!!! ??? $$$ ==============>"
+    return 'Hello World! ' +  mainpath + 'ip: ' + ip
 
 @myapp.route("/test")
 def test():
